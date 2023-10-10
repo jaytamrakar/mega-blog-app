dcb5102b382bcba3e9a9a1ad35598bd214594a9c
jaytamrakar <jaytamrakar5@gmail.com>
2023-10-11 02:35:32 +0530

    all pages and form setup complete

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,26 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import PostForm from "./post-form/PostForm";
+import AuthLayout from "./AuthLayout";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, RTE };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Input,
+  Select,
+  RTE,
+  Signup,
+  Login,
+  PostCard,
+  PostForm,
+  AuthLayout,
+};
